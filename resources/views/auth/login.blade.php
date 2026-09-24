45d4b9a038285a6c680777810c213db670bf3b3e
Pandunanda P. Kafah <pandukyouka@gmail.com>
2026-09-24 23:33:04 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                            required
                            autofocus
                            autocomplete="username"
-                           placeholder="nama@smart.local"
+                           placeholder="nama@unisayogya.ac.id"
                            class="w-full pl-9 pr-3 py-2 text-xs bg-gray-50/50 border border-gray-300 rounded-lg focus:bg-white focus:ring-1 focus:ring-[#002147] focus:border-[#002147] transition-all text-gray-900 placeholder:text-gray-400">
                 </div>
                 <x-input-error :messages="$errors->get('email')" class="mt-1.5 text-xs text-red-600" />
